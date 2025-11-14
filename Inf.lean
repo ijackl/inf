9f29a8d3749b66d6d827ffefaf979b8e2f0e9152
ijackl <43089859+ijackl@users.noreply.github.com>
2025-11-14 20:57:23 +0100

    Get rid of default setup

--- Inf.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `Inf` library.
--- Import modules here that should be built as part of the library.
-import Inf.Basic
--- Main.lean
+++
@@ -1,4 +0,0 @@
-import Inf
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
